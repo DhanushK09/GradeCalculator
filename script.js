e7d60b7bb47189dd23a4c6dd1b7722f8fd9b5b36
DhanushK09 <dhanushk2036@gmail.com>
2026-06-21 00:37:33 +0530

    colors added

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ function calculate() {
 
     let marks = document.getElementById("marks").value;
     let grade;
+    let color;
 
     if (marks >= 90) {
         grade = "A";
+        color = "lightgreen";
     }
     else if (marks >= 75) {
         grade = "B";
+        color = "yellow";
     }
     else if (marks >= 50) {
         grade = "C";
+        color = "orange";
     }
     else {
         grade = "F";
+        color = "red";
     }
 
-    document.getElementById("result").innerText =
-        "Your Grade: " + grade;
+    let result = document.getElementById("result");
+
+    result.innerText = "Your Grade: " + grade;
+    result.style.color = color;
 }
